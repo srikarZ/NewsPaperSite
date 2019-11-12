d734224b35db963414f046985515d1ac555ac160
Srikar Reddy Ramesh Kumar <srameshkumar@nyc.yearup.org>
2019-11-12 12:44:29 -0500

    Added mouseover and mouseleave to main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var y= document.getElementById("vid")
+    y.addEventListener("mouseover", function() {
+      y.style.width= '650px'
+      y.style.height='400px'
+    })
+  y.addEventListener("mouseleave", function() {
+    y.style.width= '560px'
+    y.style.height='315px'
+  })
